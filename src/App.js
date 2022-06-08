310b32c98f18f6f89bc0ed17066100cf8b77d750
Alexco888 <contacto@cafedelossentidos.com>
2022-06-08 01:29:10 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoriesSection from "./components/categories-section/CategoriesSection";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <CategoriesSection />;
+};
 
 export default App;
